162b3e3d17084007b563eccba14b2d5d3424273a
Tommy3DE <84966413+Tommy3DE@users.noreply.github.com>
2023-05-16 23:13:28 +0200

    fixes in building and refreshed spreadsheet

--- src/components/substrony/PrzykladowaKartaMieszkania.tsx
+++ src/components/substrony/PrzykladowaKartaMieszkania.tsx
@@ -1,7 +1,10 @@
 import SubNav from "./SubNav";
 import { noweMieszkania } from "../../data/noweMieszkania";
 import { useState } from "react";
-import spacerPlaceholder from '../../assets/3dplaceholder.png'
+import spacerPlaceholder from "../../assets/3dplaceholder.png";
+import FormularzKontaktowy from "../FormularzKontaktowy";
+import { Link } from "react-scroll";
+
 export const PrzykladowaKartaMieszkania = () => {
   const [activeButton, setActiveButton] = useState(0);
   const handleClick = (index: number) => {
@@ -15,18 +18,46 @@ export const PrzykladowaKartaMieszkania = () => {
         <div className="h-full w-1/2">
           <div className="mt-14 ml-14">
             <h3 className="text-lg">Osiedle Jaspisowa</h3>
-            <h1 className="text-4xl font-bold">
+            <h1 className="text-4xl font-extrabold">
               Mieszkanie {noweMieszkania[0].num}
             </h1>
-            <h2 className="text-xl">Dostępność: {noweMieszkania[0].status}</h2>
-            <div className="border-2 bg-slate-400 flex flex-col w-4/5">
-              <p>Numer mieszkania: {noweMieszkania[0].num}</p>
-              <p>Metraż: {noweMieszkania[0].pow} m²</p>
-              <p>Budynek: {noweMieszkania[0].budynek}</p>
-              <p>Kondygnacja: {noweMieszkania[0].kondygnacja}</p>
-              <p>Ilość pomieszczeń: {noweMieszkania[0].pokoje}</p>
-              <p>Powierzchnia ogródka: {noweMieszkania[0].ogrod} m²</p>
+            <h2 className="text-xl pt-4 pb-2">
+              Dostępność: {noweMieszkania[0].status}
+            </h2>
+            <div className="border-2 flex flex-row w-8/12 text-lg">
+              <ul className="w-8/12 font-bold">
+                <li className="border p-1">Numer mieszkania</li>
+                <li className="border p-1 bg-gray-100">Metraż</li>
+                <li className="border p-1">Budynek</li>
+                <li className="border p-1 bg-gray-100">Kondygnacja</li>
+                <li className="border p-1">Ilość pomieszczeń</li>
+                <li className="border p-1 bg-gray-100">Powierzchnia ogródka</li>
+              </ul>
+              <ul className="w-4/12">
+                <li className="border p-1">{noweMieszkania[0].num}</li>
+                <li className="border p-1 bg-gray-100">
+                  {noweMieszkania[0].pow} m²
+                </li>
+                <li className="border p-1">{noweMieszkania[0].budynek}</li>
+                <li className="border p-1 bg-gray-100">
+                  {noweMieszkania[0].kondygnacja}
+                </li>
+                <li className="border p-1">{noweMieszkania[0].pokoje}</li>
+                <li className="border p-1 bg-gray-100">
+                  {noweMieszkania[0].ogrod} m²
+                </li>
+              </ul>
             </div>
+            <Link to="kontakt" smooth={true} duration={1500}>
+            <button className="w-8/12 bg-[#2f917e] py-2 text-white text-lg my-4 hover:bg-slate-600">
+              Zapytaj o mieszkanie
+            </button>
+            </Link>
+            <a href={noweMieszkania[0].rzut} download={true}>
+              <button className="w-8/12 bg-gray-400 py-2 text-white text-lg hover:bg-slate-600">
+                Pobierz rzut 2D
+              </button>
+            </a>
           </div>
         </div>
         <div className="h-full w-1/2 border">
@@ -66,14 +97,26 @@ export const PrzykladowaKartaMieszkania = () => {
           )}
           {activeButton === 1 && (
             <a href={noweMieszkania[0].spacer} target="_blank">
-                <img src={spacerPlaceholder} alt="3destate spacer" className="w-full h-full" />
+              <img
+                src={spacerPlaceholder}
+                alt="3destate spacer"
+                className="w-full h-[96%]"
+              />
             </a>
           )}
           {activeButton === 2 && (
-            <img src={noweMieszkania[0].rzut} alt="rzut2d" className="h-[90%] mx-auto" />
+            <img
+              src={noweMieszkania[0].rzut}
+              alt="rzut2d"
+              className="h-[90%] mx-auto"
+            />
           )}
         </div>
+
       </div>
+
+      <FormularzKontaktowy/>
     </section>
+    
   );
 };
