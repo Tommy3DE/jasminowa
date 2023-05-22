5685260bc4d77d2bfa56b31abe27886b903e6a16
Tommy3DE <84966413+Tommy3DE@users.noreply.github.com>
2023-05-22 19:34:35 +0200

    rwd fla ist changes

--- src/components/FlatsNew.tsx
+++ src/components/FlatsNew.tsx
@@ -4,48 +4,40 @@ import { Element } from "react-scroll";
 import { Link } from "react-router-dom";
 import "./Atuty.css";
 const FlatsNew = () => {
+  const tytuly = ['Numer', 'Pow.', 'Ogródek', 'Piętro', 'Pokoje', 'Status', 'Katalog', 'Więcej']
   return (
     <Element className="cinzel w-full h-auto bg-[#2f917e]" name="mieszkania">
       <h1 className="lg:text-5xl text-white text-center py-10 text-3xl">
         Dostępne mieszkania
       </h1>
-      <ul className="lg:flex justify-between w-1/2 items-center text-md text-white mx-auto hidden">
-        <li className="w-[12.5%] text-center">Numer</li>
-        <li className="w-[12.5%] text-center">Pow.</li>
-        <li className="w-[12.5%] text-center">Ogródek</li>
-        <li className="w-[12.5%] text-center">Piętro</li>
-        <li className="w-[12.5%] text-center">Pokoje</li>
-        <li className="w-[12.5%] text-center">Status</li>
-        <li className="w-[12.5%] text-center">
-          Katalog 
-        </li>
-        <li className="w-[12.5%] text-center">
-          Więcej
-        </li>
+      <ul className="lg:flex justify-between lg:w-1/2 w-full items-center text-md text-white mx-auto hidden">
+        {tytuly.map((tytul)=> (
+        <li className="w-[12.5%] text-center">{tytul}</li>
+        ))}
       </ul>
-      <ul className="text-black mx-auto py-3">
+      <ul className="text-black mx-auto py-3 overflow-x-scroll">
         {noweMieszkania.map((flat) => (
           <li
-            className="lg:w-1/2 mx-auto bg-slate-50 flex lg:justify-between justify-evenly sm:justify-between py-7 my-3 items-center w-full md:text-md text-xs lg:text-lg"
+            className="lg:w-1/2 mx-auto bg-slate-50 flex lg:justify-between justify-evenly sm:justify-between py-7 my-3 items-center w-full  md:text-md text-xs lg:text-lg"
             key={flat.num}
           >
-            <div className="w-[12.5%] text-center">
+            <div className="w-[12.5%] text-center ">
               <p>{flat.num}</p>
             </div>
             <div className="w-[12.5%] text-center">
               <p>{flat.pow} m²</p>
             </div>
-            <div className="w-[12.5%] text-center">
+            <div className="w-[12.5%] text-center lg:block hidden">
               <p>{!flat.ogrod ? "brak" : flat.ogrod + ' m²'}</p>
             </div>
-            <div className="w-[12.5%] text-center">
+            <div className="w-[12.5%] text-center lg:block hidden">
               <p>{flat.kondygnacja}</p>
             </div>
-            <div className="w-[12.5%] text-center">
+            <div className="w-[12.5%] text-center lg:block hidden">
               <p>{flat.pokoje}</p>
             </div>
             
-            <div className="w-[12.5%] text-center ">
+            <div className="w-[12.5%] text-center">
               <p
                 className={`${flat.status === "Wolne" && "text-green-500"} ${
                   flat.status === "Sprzedane" && "text-red-600"
@@ -57,7 +49,7 @@ const FlatsNew = () => {
                 {flat.status}
               </p>
             </div>
-            <div className="w-[12.5%] text-center">
+            <div className="w-[12.5%] text-center lg:block hidden">
               <a
                 className="text-sm  border-black border-[1px] md:p-1 rounded-sm atagbtn ml-4"
                 href={flat.rzut}
@@ -67,7 +59,7 @@ const FlatsNew = () => {
               </a>
             </div>
             <Link className="w-[12.5%] text-center" to={`/mieszkanie/${flat.num}`}>
-              <p className="text-sm  border-black border-[1px] md:py-1 rounded-sm atagbtn ml-4">
+              <p className="md:text-sm  border-black border-[1px] md:py-1 rounded-sm atagbtnlg lg:mx-4 text-xs ">
                 Więcej
               </p>
             </Link>
