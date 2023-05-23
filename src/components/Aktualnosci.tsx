40e89fd3d56cf0834e101fcc9b26ec57d73ae0ce
Tommy3DE <84966413+Tommy3DE@users.noreply.github.com>
2023-05-23 21:20:38 +0200

    Aktuanosci component optimization

--- src/components/Aktualnosci.tsx
+++ src/components/Aktualnosci.tsx
@@ -1,27 +1,34 @@
-import React from 'react'
-import { posty } from '../data/posty'
+import React from "react";
+import { posty } from "../data/posty";
 
 const Aktualnosci = () => {
   return (
-    <section className='mx-auto w-full cinzel px-3'>
-        <h1 className='text-[#2f917e] lg:text-5xl text-3xl text-center my-20'>Aktualności</h1>
-        <ul>
-            {posty.map((post)=> (
-                <li className='flex mb-20 lg:flex-row flex-col' key={post.tytul}>
-                <img src={post.img} alt={post.data} className='xl:w-[333px] h-auto' />
-                <div className='flex flex-col justify-start items-start px-5'>
-                  <h2 className='font-semibold text-lg'>{post.tytul}</h2>
-                  <p className='text-sm text-gray-500 my-5'>{post.data}</p>
-                  <p>{post.text1}</p>
-                  {post.text2 && post.text2.split('\n').map((item, index) => <p key={index} className='mt-5'>{item}</p>)}
-                  {post.text3 && <p className='mt-5'>{post.text3}</p>}
-                  {post.text4 && <p className='mt-5'>{post.text4}</p>}
-                </div>
-              </li>
-            ))}
-        </ul>
+    <section className="mx-auto w-full cinzel px-3">
+      <h1 className="text-[#2f917e] lg:text-5xl text-3xl text-center my-20">
+        Aktualności
+      </h1>
+      <ul>
+        {posty.map(({ tytul, img, data, text1, text2, text3, text4 }) => (
+          <li className="flex mb-20 lg:flex-row flex-col" key={tytul}>
+            <img src={img} alt={data} className="xl:w-[333px] h-auto" />
+            <div className="flex flex-col justify-start items-start px-5">
+              <h2 className="font-semibold text-lg">{tytul}</h2>
+              <p className="text-sm text-gray-500 my-5">{data}</p>
+              <p>{text1}</p>
+              {text2 &&
+                text2.split("\n").map((item, index) => (
+                  <p key={index} className="mt-5">
+                    {item}
+                  </p>
+                ))}
+              {text3 && <p className="mt-5">{text3}</p>}
+              {text4 && <p className="mt-5">{text4}</p>}
+            </div>
+          </li>
+        ))}
+      </ul>
     </section>
-  )
-}
+  );
+};
 
-export default Aktualnosci
+export default Aktualnosci;
